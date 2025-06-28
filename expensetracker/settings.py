d79613b831fcfeb81a8e6fa7cda2c555de26ec99
ZabeeAhamed <zabeeahamed628@gmail.com>
2025-06-28 19:50:07 +0530

    Added html files

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trackerDb',
+        'USER':'postgres',
+        'PASSWORD':'postgres1',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -117,8 +121,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "hotels/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
