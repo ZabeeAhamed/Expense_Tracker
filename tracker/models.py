e6cf6beb08349e45c4e7f5a39670dd77801bf2bb
ZabeeAhamed <zabeeahamed628@gmail.com>
2025-06-29 11:56:11 +0530

    Added logical functioning

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Currentbalance(models.Model):
+    currentbalance=models.FloatField(default = 0)
+
+
+class TrackingHistory(models.Model):
+    currentbalance=models.ForeignKey(Currentbalance,on_delete=models.CASCADE)
+    amount=models.FloatField()
+    choices=(('CREDIT','CREDIT'),('DEBIT','DEBIT'))
+    expensestype=models.CharField(choices,max_length=200)
+    description=models.CharField(max_length=200)
+    created_at=models.DateTimeField(auto_now=True)
+    created_at=models.DateTimeField(auto_now_add=True)
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Currentbalance,TrackingHistory
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    if request.method=="POST":
+        description=request.POST.get("description")
+        amount=request.POST.get("amount")
+
+        currentbalance,_=Currentbalance.objects.get_or_create(id=1)
+        expensestype="CREDIT"
+
+        if float(amount)<0:
+            expensestype="DEBIT"
+        trackinghistory=TrackingHistory.objects.create(
+            currentbalance=currentbalance,
+            amount=amount,
+            expensestype=expensestype,
+            description=description
+        )
+        currentbalance.currentbalance +=float(trackinghistory.amount)
+        currentbalance.save()
+        print(description,amount)
+        return redirect('/')
+    currentbalance,_=Currentbalance.objects.get_or_create(id=1)
+    income=0
+    expense=0
+    for trackinghistory in TrackingHistory.objects.all():
+        if trackinghistory.expensestype == "CREDIT":
+            income+=trackinghistory.amount
+        else:
+            expense+=trackinghistory.amount
+    contest={'transactions' :TrackingHistory.objects.all(),'currentbalance':currentbalance,'income':income,'expense':expense}
+    return render(request,"index.html",contest)
+
+def delete_transaction(request,id):
+    trackinghistory=TrackingHistory.objects.filter(id=id)
+    if trackinghistory.exists():
+        trackinghistory=trackinghistory[0]
+        currentbalance,_=Currentbalance.objects.get_or_create(id=1)
+        currentbalance.currentbalance -= trackinghistory.amount
+        currentbalance.save();
+
+
+    trackinghistory.delete()
+    return redirect('/')
